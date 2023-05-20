cbd527442473fe00f53b41d0f9890985cc1cc35c
k.k <106207231+funyanya123@users.noreply.github.com>
2023-05-21 02:43:45 +0900

    Update Fileup.js

--- Fileup.js
+++ Fileup.js
@@ -1,13 +1,45 @@
-const handleFileSelect = () => {
-  const fileInput = document.getElementById('up');
-  const files = fileInput.files;
-  for (let i = 0; i < files.length; i++) {
-    const file = files[i];
-    const imageURL = URL.createObjectURL(file);
-    console.log(imageURL);
-    // ここで得られたimageURLを使って処理を行うことができます
-  }
-}
+(function() {
+  var form = document.getElementById('uploadForm');
+  var input = document.getElementById('avatarInput');
 
-const fileInput = document.getElementById('up');
-fileInput.addEventListener('change', handleFileSelect);
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    var file = input.files[0];
+    var randomURL = generateRandomURL();
+
+    // ここでランダムなURLを使用して何らかの処理を行う
+    console.log('ランダムなURL:', randomURL);
+    
+    // 画像をアップロードするための処理を実行する
+    uploadImage(file, randomURL);
+  });
+
+  function generateRandomURL() {
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var length = 8;
+    var randomURL = '';
+
+    for (var i = 0; i < length; i++) {
+      randomURL += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return randomURL;
+  }
+
+  function uploadImage(file, randomURL) {
+    var formData = new FormData();
+    formData.append('avatar', file);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'http://example.com/upload/' + randomURL);
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        console.log('画像のアップロードに成功しました');
+      } else {
+        console.log('画像のアップロードに失敗しました');
+      }
+    };
+    xhr.send(formData);
+  }
+})();
