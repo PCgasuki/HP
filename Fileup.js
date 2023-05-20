9c2f8847346222f8a6793735857ea166953d7832
k.k <106207231+funyanya123@users.noreply.github.com>
2023-05-21 02:39:09 +0900

    Update Fileup.js

--- Fileup.js
+++ Fileup.js
@@ -1,13 +1,13 @@
- const handleFileSelect = () => {
-        const fileInput = document.getElementById('up');
-        const files = fileInput.files;
-        for (let i = 0; i < files.length; i++) {
-          const file = files[i];
-          const imageURL = URL.createObjectURL(file);
-          console.log(imageURL);
-          // ここで得られたimageURLを使って処理を行うことができます
-        }
-      }
+const handleFileSelect = () => {
+  const fileInput = document.getElementById('up');
+  const files = fileInput.files;
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const imageURL = URL.createObjectURL(file);
+    console.log(imageURL);
+    // ここで得られたimageURLを使って処理を行うことができます
+  }
+}
 
-      const fileInput = document.getElementById('up');
-      fileInput.addEventListener('change', handleFileSelect);
+const fileInput = document.getElementById('up');
+fileInput.addEventListener('change', handleFileSelect);
